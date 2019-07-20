590d21cb030de57b4596fef7cfc733e26145055e
暮雨 <luchunyu@luchunyu.lan>
2019-07-21 01:16:43 +0800

    上传题目

--- leetcode-148/110.balanced-binary-tree.py
+++ leetcode-148/110.balanced-binary-tree.py
@@ -12,32 +12,32 @@
 # Testcase Example:  '[3,9,20,null,null,15,7]'
 #
 # Given a binary tree, determine if it is height-balanced.
-# 
+#
 # For this problem, a height-balanced binary tree is defined as:
-# 
-# 
+#
+#
 # a binary tree in which the depth of the two subtrees of every node never
 # differ by more than 1.
-# 
-# 
+#
+#
 # Example 1:
-# 
+#
 # Given the following tree [3,9,20,null,null,15,7]:
-# 
-# 
+#
+#
 # ⁠   3
 # ⁠  / \
 # ⁠ 9  20
 # ⁠   /  \
 # ⁠  15   7
-# 
+#
 # Return true.
-# 
+#
 # Example 2:
-# 
+#
 # Given the following tree [1,2,2,3,3,null,null,4,4]:
-# 
-# 
+#
+#
 # ⁠      1
 # ⁠     / \
 # ⁠    2   2
@@ -45,10 +45,10 @@
 # ⁠  3   3
 # ⁠ / \
 # ⁠4   4
-# 
-# 
+#
+#
 # Return false.
-# 
+#
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -57,6 +57,24 @@
 #         self.left = None
 #         self.right = None
 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   3
+# ⁠   / \   \
+# ⁠  4   5   6
+# ⁠ /         \
+# ⁠7           8
+
+import math
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        
+        if not root:
+            return True
+        if abs(self.getHeight(root.left) - self.getHeight(root.right)) > 1:
+            return False
+        return  self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return max(self.getHeight(root.left), self.getHeight(root.right)) + 1
