fce03588fb6331d93ed7999801d32a69de3d0875
muyurainy <tjulcy@163.com>
2019-07-28 22:03:52 -0400

    上传题目_0728

--- leetcode-148/137.single-number-ii.py
+++ leetcode-148/137.single-number-ii.py
@@ -13,26 +13,44 @@
 #
 # Given a non-empty array of integers, every element appears three times except
 # for one, which appears exactly once. Find that single one.
-# 
+#
 # Note:
-# 
+#
 # Your algorithm should have a linear runtime complexity. Could you implement
 # it without using extra memory?
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: [2,2,3,2]
 # Output: 3
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: [0,1,0,1,0,1,99]
 # Output: 99
-# 
+#
 #
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
+        one, two = 0, 0
+        for i in range(len(nums)):
+            one = (one ^ nums[i]) & ~two
+            two = (two ^ nums[i]) & ~one
+        return one
+# 大致的思路就是统计每一数位上1的个数，如果能够能够整除3就是0，反之就是1，再次提供一种高效的做法，充分的利用了位运算并且可以扩展到出现次数大于3的情况
+'''
+class solution:
+    # 四个重复出现的
+    def singleNumber(self, nums):
+        one, two, three = 0, 0, 0
+        for i in range(len(nums)):
+            one = (one ^ nums[i]) & ~two & ~three
+            two = (two ^ nums[i]) & ~one & ~three
+            three = (three ^ nums[i]) & ~two & ~one
+        return one, two, three
+s = solution()
+s.sigleNumber([2, 2, 2, 2, 4, 3, 3])
+'''
